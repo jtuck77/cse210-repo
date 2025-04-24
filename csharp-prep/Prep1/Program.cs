578708d5dd1e4aa00b85694e817fc2aaa0d84e5f
Tuck <jtuck@byui.edu>
2025-04-24 08:57:29 -0600

    undo small changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,14 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-
-        Console.WriteLine("Please enter a grade: ");
-        string grade = Console.ReadLine();
-
-        if (int.Parse(grade) >= 90){
-            Console.WriteLine("Your grade equates to an A");
-        }
-        
+        Console.WriteLine("Hello Prep1 World!");  
     }
 }
